52279f276d6a9ee2d83cc0a05e7f95c56c44c3ac
baiye <1159428886@qq.com>
2023-12-28 20:42:40 +0800

    fix:类型匹配

--- typescirpt/type-challenges/questions/00014-easy-first/template.ts
+++ typescirpt/type-challenges/questions/00014-easy-first/template.ts
@@ -1,1 +1,1 @@
-type First<T extends any[]> = any
+type First<T extends any[]> = T extends [infer F, ...infer rest] ? F : never;
--- typescirpt/type-challenges/questions/00043-easy-exclude/template.ts
+++ typescirpt/type-challenges/questions/00043-easy-exclude/template.ts
@@ -1,1 +1,5 @@
-type MyExclude<T, U> = any
+type MyExclude<T, U extends T> = T extends U ? never : T;
+
+// 对于使用extends关键字的条件类型（即上面的三元表达式类型），
+// 如果extends前面的参数是一个泛型类型，当传入该参数的是联合类型，
+// 则使用分配律计算最终的结果
